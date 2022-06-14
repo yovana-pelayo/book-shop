428aae2b57a22a5f0433f3df171f150c5bd9c449
Yovana Pelayo <yovana_pelayo@yahoo.com>
2022-06-14 11:26:31 -0700

    established books route and created a schema table with id, title and released as rows

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,11 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
+DROP TABLE IF EXISTS books;
+
+CREATE TABLE books(
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    title VARCHAR,
+    released VARCHAR
+   
+
+);
